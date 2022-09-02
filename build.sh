d823d6d3a46fd7f24aa40e5ba08a0652324dd5ae
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2022-09-02 23:21:30 +0200

    addded buf2 edit
    I forgor

--- build.sh
+++ build.sh
@@ -4,4 +4,8 @@ echo Enter your FuseID
 echo to exit and save press escape, and type :wq
 sleep 5
 vim main.c :30
+echo Now it's replace buf2 time, controls are the same
+sleep 5
+vim main.c :273
+
 gcc main.c ids/aes_core.c -o cmac_gen
