714cb45fd8f6158dced0b7fa0486a6530315f062
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2022-09-02 23:03:24 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-echo edit line 30 in xxx/CEX2DEX-PSP/psp_cex2dex/main.c with your own fuseid and press Y to continue
-if [ "$input" != "Y" ] && [ "$input" != "y" ]; then
 cd psp_cex2dex
+main.c
+echo Enter your FuseID
+echo to exit and save press escape, and type :wq
+sleep 5
+vim main.c :30
 gcc main.c ids/aes_core.c -o cmac_gen
