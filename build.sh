a7670554281dc78bdfeeaf20100605a2690b5d1e
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2022-09-02 23:33:04 +0200

    Update build.sh
    How did this get here?

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 cd psp_cex2dex
-main.c
 echo Enter your FuseID
 echo to exit and save press escape, and type :wq
 sleep 5
@@ -7,5 +6,4 @@ vim main.c :30
 echo Now it's replace buf2 time, controls are the same
 sleep 5
 vim main.c :273
-
 gcc main.c ids/aes_core.c -o cmac_gen
